45d2be4dd1d38bf246bdce8e9eedd98d1a19ddb4
Galso-sourya <75131149+Galso-sourya@users.noreply.github.com>
2025-12-21 20:24:59 +0530

    Update career.php

--- career.php
+++ career.php
@@ -20,6 +20,18 @@ $read = file('names.txt');
   Name: <input type="text" name="text" /> 
   <input type="submit" name="submit" />
 </form>
-
+<!--
+  to run this file-
+  1. start xampp
+  2. go to the location C:\xampp\htdocs\
+  3. create any folder like myproject
+  4. the php file name should be index.php inside this folder
+  5. a text file names.txt should be there also in same location.
+  6. http://localhost/myproject/
+  -->
 </body>
+  <!--to run this file
+  1. 
+  -->
 </html>
+
